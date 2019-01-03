2758dcf2a7e6384c1b2120035588d166dff9eb02
Joachim Martillo <Joachim.CS.Martillo@gmail.com>
2019-01-03 07:39:55 -0500

    Need to do session properly. Minor change. Fixing the ConnectionInfo methods. Trying to get user management window to appear. The prologue portion of the program seems to work. There may be some issues with session cleanup. Just need to get the hypercms part to work. Setting up the hypercms user web pages. Making the user management work. Working on view. More but most everything seems to work. Making sure that req & res are passed through the server code in lock-step. Putting in create & edit.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const uuidv5 = require('uuid/v5');
+var datetime = require('node-datetime');
 // multer handles forms posted to the
 // server
 var multer = require('multer'); // multer handles forms posted to the
@@ -11,6 +12,104 @@ var mysql = require('mysql');
 router.mysql = mysql;
 var asyncErrDomain = require('domain');
 
+function get_and_store_record_data(button, email, con, req, res) {
+    var query = null;
+    var user_uuid = null;
+    var query2 = null;
+
+    if(button == 'view') {
+        query = 'SELECT * FROM Users WHERE email=' + '\'' + email + '\'' + ';';
+        con.query(query, function (err, result) {
+            if (err) {
+                console.log("view query failed!")
+                throw err;
+            }
+            console.log("HyperCMS user data: ");
+            for (var idb = 0; idb < result.length; ++idb) {
+                console.log(result[idb]);
+            }
+            router.webStart.connHashTable.put("query+" + req.sessionID, result);
+            user_uuid = result[0].uuid;
+            query2 = 'SELECT * FROM UserRoles WHERE user_uuid=' + '\'' + user_uuid + '\'' + ';';
+            con.query(query2, function (err, result) {
+                if (err) {
+                    console.log("view query2 failed!")
+                    throw err;
+                }
+                console.log("HyperCMS user-role data: ");
+                for (var idb = 0; idb < result.length; ++idb) {
+                    console.log(result[idb]);
+                }
+                /*This is much more complex than it should be.*/
+                router.webStart.connHashTable.put("query2+" + req.sessionID, result);
+            });
+        });
+    } else if(button == 'create') {
+        query = 'SELECT * FROM Users WHERE email=' + '\'' + email + '\'' + ';';
+        con.query(query, function (err, result) {
+            if (err) {
+                console.log("crete query failed!")
+                throw err;
+            }
+            console.log("HyperCMS user data: ");
+            for (var idb = 0; idb < result.length; ++idb) {
+                console.log(result[idb]);
+            }
+            router.webStart.connHashTable.put("query+" + req.sessionID, result);
+            user_uuid = result[0].uuid;
+            query2 = 'SELECT * FROM UserRoles WHERE user_uuid=' + '\'' + user_uuid + '\'' + ';';
+            con.query(query2, function (err, result) {
+                if (err) {
+                    console.log("create query2 failed!")
+                    throw err;
+                }
+                console.log("HyperCMS user-role data: ");
+                for (var idb = 0; idb < result.length; ++idb) {
+                    console.log(result[idb]);
+                }
+                /*This is much more complex than it should be.*/
+                router.webStart.connHashTable.put("query2+" + req.sessionID, result);
+            });
+        });
+    } else if(button == 'edit') {
+        query = 'SELECT * FROM Users WHERE email=' + '\'' + email + '\'' + ';';
+        con.query(query, function (err, result) {
+            if (err) {
+                console.log("edit query failed!")
+                throw err;
+            }
+            console.log("HyperCMS user data: ");
+            for (var idb = 0; idb < result.length; ++idb) {
+                console.log(result[idb]);
+            }
+            router.webStart.connHashTable.put("query+" + req.sessionID, result);
+            user_uuid = result[0].uuid;
+            query2 = 'SELECT * FROM UserRoles WHERE user_uuid=' + '\'' + user_uuid + '\'' + ';';
+            con.query(query2, function (err, result) {
+                if (err) {
+                    console.log("edit query2 failed!")
+                    throw err;
+                }
+                console.log("HyperCMS user-role data: ");
+                for (var idb = 0; idb < result.length; ++idb) {
+                    console.log(result[idb]);
+                }
+                /*This is much more complex than it should be.*/
+                router.webStart.connHashTable.put("query2+" + req.sessionID, result);
+            });
+        });
+    }
+
+    res.render('viewpause', {title: 'User not yet ready: ' + req.sessionID}, function (err, html) {
+        if (err != null) {
+            console.log(err);
+        } else {
+            console.log(html); // the MySQL Login form
+            res.send(html);
+        }
+    });
+}
+
 // This line is executed on startup -- must be able to transform
 // router.upload.none() to a value on startup.
 router.post('/', router.upload.none(), function (req, res, next) {
@@ -19,6 +118,9 @@ router.post('/', router.upload.none(), function (req, res, next) {
     var con = router.webStart.connHashTable.get("con+" + res.req.sessionID);
     // on callback
     // these values below come from the HTTP POST'd form after login is click'd
+    var button = ((req.body.button_create == "create") ? "create" : "");
+    var button = button + ((req.body.button_edit == "edit") ? "edit" : "");
+    var button = button + ((req.body.button_view == "view") ? "view" : "");
     var hcmsuserinfo = {
         email: req.body.email,
         hcmspassword: req.body.hcmspassword,
@@ -30,9 +132,7 @@ router.post('/', router.upload.none(), function (req, res, next) {
         phone: req.body.phone,
         jobTitle: req.body.jobTitle,
         role: req.body.role,
-        button_create: req.body.button_create,
-        button_edit: req.body.button_edit,
-        button_view: req.body.button_view,
+        button: button
     };
 
     var processDataBase = asyncErrDomain.create();
@@ -41,18 +141,19 @@ router.post('/', router.upload.none(), function (req, res, next) {
         console.log("Could not get to Users datatable.")
         console.log("Is the correct MySQL server selected?")
         ss.setConnectInfoTryable(false);
-        if (res != null) // remnant of old code?
-            res.render('index', {title: 'MySQL Reconnect Page: ' + ss.sessionID}, function (err, html) {
-                if (err != null) {
-                    console.log(err);
-                } else {
-                    console.log(html); // the MySQL Login form
-                    res.send(html);
-                }
-            });
+
+        res.render('index', {title: 'MySQL Reconnect Page: ' + ss.sessionID}, function (err, html) {
+            if (err != null) {
+                console.log(err);
+            } else {
+                console.log(html); // the MySQL Login form
+                res.send(html);
+            }
+        });
     });
 
     processDataBase.run(function () {
+        /* Absence of connection to database server is insurmountable obstacle */
         if (con == undefined)
             con = null;
         if (con == null) {
@@ -68,66 +169,31 @@ router.post('/', router.upload.none(), function (req, res, next) {
                     res.send(html);
                 }
             });
-        } else if (hcmsuserinfo.button_view == 'view') {
-            if (hcmsuserinfo.email == router.webStart.userdefaults.email_ph)
+        } else if(hcmsuserinfo.email == router.webStart.userdefaults.email_ph) {
             /* Tommy Flowers doesn't exist in database. */
-                res.render('users',
-                    Object.assign(Object.assign(Object.assign({}, userdefaults),
-                        {title: "HyperCMS Bad User: " + ss.getSessionID(res.req)}),
-                        {role_ph: router.connHashTable.get("roles+" + res.req.sessionID)}),
-                    function (err, html) {
-                        if (err != null) {
-                            console.log(err);
-                        } else {
-                            // html value comes from rendering the Jade template.
-                            console.log(html);
-                            res.send(html);
-                        }
-                    });
-            else {
-                var query = 'SELECT * FROM Users WHERE email=' + '\'' + hcmsuserinfo.email + '\'' + ';';
-                con.query(query, function (err, result) {
-                    if (err) {
-                        console.log("View query failed!")
-                        throw err;
-                    }
-                    console.log("HyperCMS user data: ");
-                    for (var idb = 0; idb < result.length; ++idb) {
-                        console.log(result[idb]);
-                    }
-                    router.webStart.connHashTable.put("query+" + res.req.sessionID, result);
-                    var user_uuid = result[0].uuid;
-                    var query2 = 'SELECT * FROM UserRoles WHERE user_uuid=' + '\'' + user_uuid + '\'' + ';';
-                    con.query(query2, function (err, result) {
-                        if (err) {
-                            console.log("View query2 failed!")
-                            throw err;
-                        }
-                        console.log("HyperCMS user-role data: ");
-                        for (var idb = 0; idb < result.length; ++idb) {
-                            console.log(result[idb]);
-                        }
-                        /*This is much more complex than it should be.*/
-                        router.webStart.connHashTable.put("query2+" + res.req.sessionID, result);
-                    });
-                });
-                res.render('viewpause', {title: 'User not yet ready: ' + ss.getSessionID(res.req)},
-                    function (err, html) {
-                        if (err != null) {
-                            console.log(err);
-                        } else {
-                            console.log(html); // the MySQL Login form
-                            res.send(html);
-                        }
-                    });
-            }
-        } else if (hcmsuserinfo.button_edit == 'edit') {
-        } else if (hcmsuserinfo.button_create == 'create') {
-        } else {
+            /* I really dislike this indentation */
             res.render('users',
-                Object.assign(Object.assign(Object.assign({}, userdefaults),
+                Object.assign(Object.assign(Object.assign({}, router.webStart.userdefaults),
+                    {title: "HyperCMS Bad User: " + req.sessionID}),
+                    {role_ph: router.connHashTable.get("roles+" + req.sessionID)}),
+                function (err, html) {
+                    if (err != null) {
+                        console.log(err);
+                    } else {
+                        // html value comes from rendering the Jade template.
+                        console.log(html);
+                        res.send(html);
+                    }
+                });
+        } else if (hcmsuserinfo.button != "") {
+            router.webStart.connHashTable.put("newuserdata+" + req.sessionID, hcmsuserinfo);
+            get_and_store_record_data(button, hcmsuserinfo.email, con, req, res);
+        } else {
+            /* This code may be unnecessary because some button should be pushed. */
+            res.render('users',
+                Object.assign(Object.assign(Object.assign({}, router.webStart.userdefaults),
                     {title: "Choose Action: " + ss.getSessionID(res.req)}),
-                    {role_ph: router.webstart.connHashTable.get("roles+" + res.req.sessionID)}),
+                    {role_ph: router.webstart.connHashTable.get("roles+" + req.sessionID)}),
                 function (err, html) {
                     if (err != null) {
                         console.log(err);
@@ -144,9 +210,10 @@ router.post('/', router.upload.none(), function (req, res, next) {
 router.post('/viewpause', router.upload.none(), function (req, res, next) {
     var cio = req.session.connectInfoSession;
     var ss = new router.webStart.sessShadowBuilder(cio);
-    var queryresult = router.webStart.connHashTable.get("query+" + res.req.sessionID);
-    var queryresult2 = router.webStart.connHashTable.get("query2+" + res.req.sessionID);
-    var rolestable = router.webStart.connHashTable.get("rolesTable+" + res.req.sessionID);
+    var queryresult = router.webStart.connHashTable.get("query+" + req.sessionID);
+    var queryresult2 = router.webStart.connHashTable.get("query2+" + req.sessionID);
+    var rolestable = router.webStart.connHashTable.get("rolesTable+" + req.sessionID);
+    var hcmsuserinfo = router.webStart.connHashTable.get("newuserdata+" + req.sessionID);
 
     if ((rolestable == undefined) || (rolestable == null)) {
         console.log("Roles Table seems to be missing.");
